424bdaec88f208a2c428bbd3a26b029ef46d89d0
Marshanda Gwie <marshandasugiarto@gmail.com>
2024-04-26 08:34:18 +0700

    Add comments to each files

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,8 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Marshanda Gwie on 22/04/24.
+//
+
+import Foundation
---
+++ MacLandmarks/LandmarkSetting.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkSetting.swift
+//  MacLandmarks
+//
+//  Created by Marshanda Gwie on 22/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkSetting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkSetting()
+}
